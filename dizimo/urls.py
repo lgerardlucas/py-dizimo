c5dae7602271d1741d99a3fc9607d9cd6da2d1ad
lgerardlucas <lgerardlucas@gmail.com>
2021-09-28 21:37:06 -0300

    Ajustes no layout

--- dizimo/urls.py
+++ dizimo/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 # Admin Site Config
 admin.sites.AdminSite.site_header = 'Controle de Dizimista'
@@ -8,4 +10,4 @@ admin.sites.AdminSite.index_title = 'Controle de Dizimista'
 
 urlpatterns = [
     path('', admin.site.urls),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
