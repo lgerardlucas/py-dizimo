d8d5ee4ae2b7e7a67c08818a9e24971dd15f1cfa
lgerardlucas <lgerardlucas@gmail.com>
2021-09-29 20:59:15 -0300

    Deploy

--- dizimo/settings.py
+++ dizimo/settings.py
@@ -9,7 +9,8 @@ from decouple import config
 #BASE_DIR = Path(__file__).resolve().parent.parent
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = SECRET_KEY = config('SECRET_KEY')
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = True
+#config('DEBUG', default=False, cast=bool)
 ALLOWED_HOSTS = ['*']
 INTERNAL_IPS = ['127.0.0.1']
 
